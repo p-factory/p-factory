a779c4977757265e4158c6ab5efbc93ff9045d2a
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-29 17:08:31 +0900

    🔥: index.tsx 내부 불필요 코드 주석 처리

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    console.log('test');
+  });
   return <></>;
 };
 
