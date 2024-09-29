0d094789953b0028addf86c7cbfd1cfa8de973c1
[nyun-nye] <[2001_11_7@naver.com]>
2024-09-30 01:54:05 +0900

    ✨: font-size, font-weight 적용

--- front/src/View/devTermial.tsx
+++ front/src/View/devTermial.tsx
@@ -5,7 +5,37 @@ const DevTerminal = () => {
     <div>
       <h1>Dev Terminal Page</h1>
       <p>테스트를 위한 페이지 입니다.</p>
-      <p className="font-JejuDoldam">제주돌담체 폰트 적용</p>
+      <p className="text-xl font-bold font-JejuDoldam">
+        제주돌담체 폰트 적용 Bold 24px
+      </p>
+      <p className="font-semibold text-l font-JejuDoldam">
+        제주돌담체 폰트 적용 SemiBold 22px
+      </p>
+      <p className="font-medium text-m font-JejuDoldam">
+        제주돌담체 폰트 적용 Medium 20px
+      </p>
+      <p className="font-normal text-s font-JejuDoldam">
+        제주돌담체 폰트 적용 Regular 18px
+      </p>
+      <p className="text-xs font-light font-JejuDoldam">
+        제주돌담체 폰트 적용 Light 16px
+      </p>
+      <hr />
+      <p className="text-xl font-bold font-Pretendard">
+        Pretendard 폰트 적용 Bold 24px
+      </p>
+      <p className="font-semibold text-l font-Pretendard">
+        Pretendard 폰트 적용 SemiBold 22px
+      </p>
+      <p className="font-medium text-m font-Pretendard">
+        Pretendard 폰트 적용 Medium 20px
+      </p>
+      <p className="font-normal text-s font-Pretendard">
+        Pretendard 폰트 적용 Regular 18px
+      </p>
+      <p className="text-xs font-light font-Pretendard">
+        Pretendard 폰트 적용 Light 16px
+      </p>
     </div>
   );
 };
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,9 +2,20 @@
 module.exports = {
   content: ['src/**/*.{html,js,jsx,ts,tsx,css,scss}', 'public/index.html'],
   theme: {
-    extend: {},
-    fontFamily:{
-      JejuDoldam: ['Jeju Doldam'],
+    extend: {
+      /* text */
+      fontSize: {
+        'xl': '24px',
+        'l': '22px',
+        'm': '20px',
+        's': '18px',
+        'xs': '16px',
+      },
+      /* font */
+      fontFamily: {
+        JejuDoldam: ['Jeju Doldam'],
+        Pretendard: ['Pretendard'],
+      },
     },
   },
   plugins: [],
