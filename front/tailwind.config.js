669afa564f5430b4dfe8c6e9808e7de18baf376b
nyun-nye <138736197+nyun-nye@users.noreply.github.com>
2024-09-30 15:25:02 +0900

    🔀: Merge branch `feature/14` into `develop`
    * 🔥: logo.svg 제거
    
    * 🔥: App.tsx 내부 코드 삭제
    
    * 🔥: index.tsx 내부 불필요 코드 주석 처리
    
    * 🔧: react-router-dom 설치
    
    * 🔥: PWA 관련 파일 삭제
    
    * 🔥: index.html 불필요한 코드 삭제
    
    * 🔥: App.css 불필요한 코드 삭제
    
    * 🔥: App.tsx 불필요한 코드 삭제
    
    * 🔥: index.tsx 불필요한 코드 삭제
    
    * ✨: tailwindcss에 font 적용 및 작동 확인
    
    * ✨: 제주돌담체 폰트 적용
    
    * ✨: font-size, font-weight 적용
    
    * ✨: tailwind로 color 설정
    
    * ✨: 필요 코드 복구 및 dev페이지 이름 변경
    
    ---------
    
    Co-authored-by: [nyun-nye] <[2001_11_7@naver.com]>

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,7 +2,40 @@
 module.exports = {
   content: ['src/**/*.{html,js,jsx,ts,tsx,css,scss}', 'public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      /* font-size */
+      fontSize: {
+        'xl': '24px',
+        'l': '22px',
+        'm': '20px',
+        's': '18px',
+        'xs': '16px',
+      },
+      /* font */
+      fontFamily: {
+        JejuDoldam: ['Jeju Doldam'],
+        Pretendard: ['Pretendard'],
+      },
+      /* font-color */
+      colors: {
+        'primaryColor': '#BAFF4A',
+        'subColor': '#000000',
+        'statusColor': {
+          '01': '#000000',
+          '02': '#222222',
+          '03': '#3E3E3E',
+          '04': '#666666',
+          '05': '#888888',
+          '06': '#A6A6A6',
+          '07': '#CCCCCC',
+          '08': '#FFFFFF',
+        },
+        'errorColor': '#FF0000',
+        'successColor': '#0038FF',
+        'safetyColor': '#32BF00',
+        'neutralColor': '#656565',
+      },
+    },
   },
   plugins: [],
 };
