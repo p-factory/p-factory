afb33d5f88efcca71a6c6fdd263cecb697d8378a
4Bee <4bee.code@gmail.com>
2024-10-04 00:04:28 +0900

    💄: font관련 영역 utils 네이밍으로 분리 및 모듈 객체화

---
+++ front/src/styles/fontStyles.utils.js
@@ -0,0 +1,14 @@
+const fontStyles = {
+  '.JejuDoldam': { fontFamily: "'Jeju Doldam', 'sans- serif'" },
+  '.Pretendard': { fontFamily: "'Pretendard', 'sans-serif'" },
+};
+
+const fontSizes = {
+  '.font-xl': { fontSize: 'var(--font-xl)' },
+  '.font-l': { fontSize: 'var(--font-l)' },
+  '.font-m': { fontSize: 'var(--font-m)' },
+  '.font-s': { fontSize: 'var(--font-s)' },
+  '.font-xs': { fontSize: 'var(--font-xs)' },
+};
+
+export default { fontStyles, fontSizes };
