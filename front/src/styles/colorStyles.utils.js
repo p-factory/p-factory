0d7c66585ea8c8261d9fdfc72fbf10c232d516da
4Bee <4bee.code@gmail.com>
2024-10-04 00:04:02 +0900

    💄: color 영역 utils 네이밍으로 분리

---
+++ front/src/styles/colorStyles.utils.js
@@ -0,0 +1,38 @@
+const colorUtils = {
+  // text Colors
+  '.text-primaryColor': { color: 'var(--primary-color)' },
+  '.text-subColor': { color: 'var(--sub-color)' },
+  '.text-errorColor': { color: 'var(--error-color)' },
+  '.text-successColor': { color: 'var(--success-color)' },
+  '.text-safetyColor': { color: 'var(--safety-color)' },
+  '.text-neutralColor': { color: 'var(--neutral-color)' },
+  // text-Status Colors
+  '.text-statusColor-01': { color: 'var(--status-color-01)' },
+  '.text-statusColor-02': { color: 'var(--status-color-02)' },
+  '.text-statusColor-03': { color: 'var(--status-color-03)' },
+  '.text-statusColor-04': { color: 'var(--status-color-04)' },
+  '.text-statusColor-05': { color: 'var(--status-color-05)' },
+  '.text-statusColor-06': { color: 'var(--status-color-06)' },
+  '.text-statusColor-07': { color: 'var(--status-color-07)' },
+  '.text-statusColor-08': { color: 'var(--status-color-08)' },
+
+  //bg Colors
+  '.bg-primaryColor': { backgroundColor: 'var(--primary-color)' },
+  '.bg-subColor': { backgroundColor: 'var(--sub-color)' },
+  '.bg-errorColor': { backgroundColor: 'var(--error-color)' },
+  '.bg-successColor': { backgroundColor: 'var(--success-color)' },
+  '.bg-safetyColor': { backgroundColor: 'var(--safety-color)' },
+  '.bg-neutralColor': { backgroundColor: 'var(--neutral-color)' },
+
+  // bg-Status Colors
+  '.bg-statusColor-01': { backgroundColor: 'var(--status-color-01)' },
+  '.bg-statusColor-02': { backgroundColor: 'var(--status-color-02)' },
+  '.bg-statusColor-03': { backgroundColor: 'var(--status-color-03)' },
+  '.bg-statusColor-04': { backgroundColor: 'var(--status-color-04)' },
+  '.bg-statusColor-05': { backgroundColor: 'var(--status-color-05)' },
+  '.bg-statusColor-06': { backgroundColor: 'var(--status-color-06)' },
+  '.bg-statusColor-07': { backgroundColor: 'var(--status-color-07)' },
+  '.bg-statusColor-08': { backgroundColor: 'var(--status-color-08)' },
+};
+
+export default colorUtils;
