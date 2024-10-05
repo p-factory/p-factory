53b8d4402d37f6e062e2e2021d8221345f6d0292
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-05 17:16:05 +0900

    💄: HowToUse 페이지 글씨 스타일 작업 완료

--- front/src/View/P-FactoryDetail/HowToUse.tsx
+++ front/src/View/P-FactoryDetail/HowToUse.tsx
@@ -2,21 +2,56 @@ import React from 'react';
 
 const HowToUse = () => {
   return (
-    <div className="--root-flex">
+    <div className="--root-flex Pretendard">
       <div className="--primary-flex-col w-1500">
-        <div>ㅍ토리 사용법</div>
+        <div className="text-[60px] JejuDoldam">ㅍ토리 사용법</div>
         <div>
-          <div>나 토리! 이 공장의 초절정 귀염둥이 마스코트지</div>
-          <div>
+          <div className="--font-m medium">
+            나 <span className="bold">토리!</span> 이 공장의 초절정 귀염둥이
+            마스코트지
+          </div>
+          <div className="--font-xs medium text-statusColor-05">
             ㅍ토리를 어떻게 사용하는지 모르겠다고? 이래서 MZ란.. 한 번만
             알려준다 따라와!!!
           </div>
         </div>
         <div>
           <div>
-            <div>어디서든 편리하고 재밌게!</div>
-            <div>월요일 아침 지옥철 안에서도!</div>
-            <div>인터넷 안되는 장거리 비행 속에서도!</div>
+            <div className="--font-xl medium">
+              어디서든 <br />
+              <span className="bold">편리하고 재밌게!</span>
+            </div>
+            <div className="--font-xs medium">
+              <div>
+                월요일 아침 <br />
+                지옥철 안에서도!
+              </div>
+              <div>
+                인터넷 안되는 <br />
+                장거리 비행 속에서도!
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="--font-xl medium">
+              다양햔 단어장을 통한 <br />
+              <span className="bold">학습효과 극대화!</span>
+            </div>
+            <div className="--font-xs medium">
+              <div>
+                친구네 공장에 <br />
+                구경갈 수도 있어요!
+              </div>
+              <div>
+                여러 카테고리의 <br />
+                단어장을 통해 <br />
+                학습목표에 맞추어 공부하세요!
+              </div>
+            </div>
+          </div>
+          <div className="btn-secondary">단어 공장 작업 시작하기</div>
+          <div className="text-statusColor-05 medium --font-xs">
+            Click me!!! 당신만의 단어 공장, 지금 바로 작업해 보세요!
           </div>
         </div>
       </div>
