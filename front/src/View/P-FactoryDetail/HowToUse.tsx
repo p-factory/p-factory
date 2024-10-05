77f72161249e5be0f76773a44441058c3c4684a1
[nyun-nye] <[2001_11_7@naver.com]>
2024-10-05 23:21:16 +0900

    💄: HowToUse 박스 내부 디자인 초안 설정 및 내부 원 생성

--- front/src/View/P-FactoryDetail/HowToUse.tsx
+++ front/src/View/P-FactoryDetail/HowToUse.tsx
@@ -3,35 +3,42 @@ import spannerIconWhite from '../../global/Img/spannerIconWhite.svg';
 
 const HowToUse = () => {
   return (
-    <div className="--root-flex Pretendard">
+    <div className="h-auto --root-flex Pretendard">
       <div className="--primary-flex-col w-1500">
-        <div className="text-[60px] JejuDoldam">ㅍ토리 사용법</div>
+        <div className="text-[60px] JejuDoldam mt-[202px] mb-[130px]">
+          ㅍ토리 사용법
+        </div>
         <div>
           <div className="--font-m medium">
-            나 <span className="bold">토리!</span> 이 공장의 초절정 귀염둥이
-            마스코트지
+            나<span className="bold bg-primaryColor"> 토리!</span> 이 공장의
+            초절정 귀염둥이 마스코트지
           </div>
-          <div className="--font-xs medium text-statusColor-05">
+          <div className="--font-xs medium text-statusColor-05 mb-[40px]">
             ㅍ토리를 어떻게 사용하는지 모르겠다고? 이래서 MZ란.. 한 번만
             알려준다 따라와!!!
           </div>
         </div>
         <div>
-          <div>
-            <div className="--font-xl medium">
-              어디서든 <br />
-              <span className="bold">편리하고 재밌게!</span>
-            </div>
-            <div className="--font-xs medium">
-              <div>
-                월요일 아침 <br />
-                지옥철 안에서도!
+          <div className="relative">
+            <div className="w-[1139px] h-[414px] border-[1px] bg-white border-black border-solid rounded-[43px] relative z-10">
+              <div className="w-[38px] h-[38px] bg-black rounded-full absolute z-10 top-[30px] left-[36px]" />
+              <div className="w-[26px] h-[26px] bg-primaryColor rounded-full absolute z-10 top-[23px] left-[78px]" />
+              <div className="--font-xl medium mt-[33px] ml-[122px]">
+                어디서든 <br />
+                <span className="bold">편리하고 재밌게!</span>
               </div>
-              <div>
-                인터넷 안되는 <br />
-                장거리 비행 속에서도!
+              <div className="flex --font-xs medium mt-[26px] h-[222px]">
+                <div className="rounded-[36px] border shadow-[0_0_16px_rgba(0,0,0,0.25)] w-[443px] ml-[96px] mr-[47px] pt-[26px] pl-[43px]">
+                  월요일 아침 <br />
+                  지옥철 안에서도!
+                </div>
+                <div className="rounded-[36px] border shadow-[0_0_16px_rgba(0,0,0,0.25)] w-[443px] pt-[26px] pl-[35px]">
+                  인터넷 안되는 <br />
+                  장거리 비행 속에서도!
+                </div>
               </div>
             </div>
+            <div className="border-[1px] border-black border-solid rounded-[43px] w-[1139px] h-[414px] bg-primaryColor absolute top-[26px] left-[21px] z-0" />
           </div>
           <div>
             <div className="--font-xl medium">
@@ -50,16 +57,18 @@ const HowToUse = () => {
               </div>
             </div>
           </div>
-          <div className="bold items-center btn-secondary Pretendard h-[102px] w-[682px]">
-            <div className="flex items-center">
-              <div className="w-[212px] h-[26px] --font-l flex items-center">
-                단어 공장 작업 시작하기
+          <div className="flex justify-center">
+            <div className="bold items-center btn-secondary Pretendard h-[102px] w-[682px]">
+              <div className="flex items-center">
+                <div className="w-[212px] h-[26px] --font-l flex items-center">
+                  단어 공장 작업 시작하기
+                </div>
+                <img
+                  src={spannerIconWhite}
+                  alt="Spanner Icon"
+                  className="h-[26px] w-[26px] ml-[5px]"
+                />
               </div>
-              <img
-                src={spannerIconWhite}
-                alt="Spanner Icon"
-                className="h-[26px] w-[26px] ml-[5px]"
-              />
             </div>
           </div>
           <div className="text-statusColor-05 medium --font-xs">
