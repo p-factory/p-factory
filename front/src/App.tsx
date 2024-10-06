99b752c2c1b94b6208a60e7c6d1c1d7a0d13dfde
4Bee <4bee.code@gmail.com>
2024-10-07 01:48:55 +0900

    ✨: 해당 디자인을 적용해볼 임시 페이지 라우팅 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DevStylesPreview from './View/DevStylesPreview';
+import DevDesignSystem from './View/DevDesignSystem';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" />
+        <Route path="/Dev" element={<DevDesignSystem />} />
         <Route path="/DevStylesPreview" element={<DevStylesPreview />} />
       </Routes>
     </Router>
