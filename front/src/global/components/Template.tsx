306ab618042b35a58371e457b36471c4b2480628
4Bee <4bee.code@gmail.com>
2024-10-14 12:49:36 +0900

    ✨: Template 컴포넌트의 width 값과 height 값을 동적으로 변경 가능하게 props 추가

--- front/src/global/components/Template.tsx
+++ front/src/global/components/Template.tsx
@@ -6,11 +6,22 @@ const DEV = Dev.ToolButton;
 // Zod 스키마 정의
 const ComponentSchema = Z.object({
   component: Z.custom<ComponentType>(),
+  width: Z.string().optional(),
+  height: Z.string().optional(),
+  widthBackground: Z.string().optional(),
+  heightBackground: Z.string().optional(),
 });
 
-type ComponentProps = Z.infer<typeof ComponentSchema>['component'];
+type ComponentProps = Z.infer<typeof ComponentSchema>;
 
-const Template = ({ component: Component }: { component: ComponentProps }) => {
+const Template = ({
+  component: Component,
+  width = 'w-[clamp(0px,95.71%,758px)]',
+  height = 'h-[clamp(0px,89.49%,758px)]',
+  widthBackground = 'w-[clamp(0px,95.71%,758px)]',
+  heightBackground = 'h-[clamp(0px,89.49%,847px)]',
+}: ComponentProps) => {
+  // const { component: Component, width = 'w-[clamp(0px,95.71%,758px)]', height = 'h-[clamp(0px,89.49%,758px)]' } = props;
   try {
     ComponentSchema.parse(Component);
   } catch (error) {
@@ -24,14 +35,20 @@ const Template = ({ component: Component }: { component: ComponentProps }) => {
         {/* 컨탠츠 영역 */}
         <div className="flex flex-col justify-end w-full">
           <div className="bg-white w-[108.91px] h-[40.58px] rounded-tl-[30px] rounded-tr-[30px]" />
-          <div className="flex w-[clamp(0px,95.71%,758px)] h-[clamp(0px,89.49%,758px)] bg-white pr-[clamp(0px,42px,42px)] pl-[clamp(0px,76px,76px)] rounded-tr-[36px] rounded-br-[36px] rounded-bl-[36px]">
+          <div
+            className={`flex ${width} ${height} bg-white pr-[clamp(0px,42px,42px)] pl-[clamp(0px,76px,76px)] rounded-tr-[36px] rounded-br-[36px] rounded-bl-[36px]`}
+          >
+            {/* w-[clamp(0px,95.71%,758px)] h-[clamp(0px,89.49%,758px)] */}
             <Component />
           </div>
         </div>
         {/* 뒷 배경 영역 */}
-        <div className="absolute flex flex-col top-[5px] left-[20px] z-[-1] w-full h-full">
+        <div className="absolute flex flex-col top-[10px] left-[25px] z-[-1] w-full h-full">
           <div className="bg-slate-400 w-[108.91px] h-[40.58px] rounded-tl-[30px] rounded-tr-[30px]" />
-          <div className="flex w-[clamp(0px,95.71%,758px)] h-[clamp(0px,89.49%,847px)] bg-slate-400 rounded-tr-[36px] rounded-br-[36px] rounded-bl-[36px]"></div>
+          <div
+            className={`flex ${widthBackground} ${heightBackground} bg-slate-400 rounded-tr-[36px] rounded-br-[36px] rounded-bl-[36px]`}
+          ></div>
+          {/* w-[clamp(0px,95.71%,758px)] h-[clamp(0px,89.49%,847px)] */}
         </div>
       </div>
     </div>
