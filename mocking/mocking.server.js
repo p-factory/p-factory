619a983cc7ed67c682f43278972d5b5495f2fd25
4Bee <4bee.code@gmail.com>
2024-10-21 23:29:00 +0900

    ✨: login과 signup을 할 수 있는 mocking API 구현

--- mocking/mocking.server.js
+++ mocking/mocking.server.js
@@ -1,7 +1,9 @@
 import jsonServer from 'json-server';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+// import fs from 'fs';
+import { getHyperLink } from './Routes/HyperLink.js';
+import { createRoute } from './Routes/createRoutes.js';
 
 // ESM 형식에서 __dirname 설정
 const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +12,7 @@ const __dirname = dirname(__filename);
 // JSON 파일 경로 설정
 const testDbPath = join(__dirname, 'db', 'db.test.json');
 const nameDbPath = join(__dirname, 'db', 'db.name.json');
+const userDbPath = join(__dirname, 'db', 'db.user.json');
 
 // JSON Server 설정
 const server = jsonServer.create();
@@ -18,35 +21,14 @@ server.use(middlewares);
 
 // 홈 페이지에 링크 추가
 server.get('/link', (req, res) => {
-  res.send(`
-    <h1>API Links</h1>
-    <ul>
-      <li><a href="/api/test">Test API</a></li>
-      <li><a href="/api/name">Name API</a></li>
-    </ul>
-  `);
+  res.send(getHyperLink());
 });
 
 // 각각의 JSON 파일을 라우터로 설정
-server.get('/api/test', (req, res) => {
-  fs.readFile(testDbPath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(JSON.parse(data));
-    }
-  });
-});
-
-server.get('/api/name', (req, res) => {
-  fs.readFile(nameDbPath, 'utf8', (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(JSON.parse(data));
-    }
-  });
-});
+createRoute(server, '/api/test', testDbPath);   // GET 처리
+createRoute(server, '/api/name', nameDbPath);   // GET 처리
+createRoute(server, '/api/user/signup', userDbPath);  // GET 및 POST 처리
+createRoute(server, '/api/user/login', userDbPath); // GET 및 POST 처리
 
 // 서버 실행
 const PORT = 3000;
@@ -54,4 +36,5 @@ server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
   console.log(`Test API: http://localhost:${PORT}/api/test`);
   console.log(`Name API: http://localhost:${PORT}/api/name`);
+  console.log(`SignUpUSers API API: http://localhost:${PORT}/api/user/signup`);
 });
