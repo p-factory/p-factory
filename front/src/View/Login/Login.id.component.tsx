22f418264d5c3761ef73cf185e4b55fdf457ee12
4Bee <4bee.code@gmail.com>
2024-11-04 17:53:56 +0900

    ✨: id 입력 값에 따라 동적으로 동작할 수 있게 버튼 구현

--- front/src/View/Login/Login.id.component.tsx
+++ front/src/View/Login/Login.id.component.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import loginImage from '../DEV/img/Vector.svg';
 
 const LoginId = () => {
+  const [isState, setState] = useState(false);
+  const [isValue, setValue] = useState('');
+  const [isButton, setButton] = useState(false);
+
+  // React.ChangeEvent<HTMLInputElement>
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValue(value);
+    if (value !== '') {
+      // setState(!isState);
+      setState(false);
+      console.log(value);
+      if (isState === false) {
+        setButton(true);
+      }
+    } else {
+      setButton(false);
+      setState(true);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-full pt-[12.01%] pb-[clamp(0px,10.39%,56px)] bg-yellow-300">
       <div className="flex flex-col justify-center items-center bg-orange-500 mb-[49px]">
@@ -23,21 +44,32 @@ const LoginId = () => {
             <span className="--error-font-Color">*</span>
           </div>
           <div className="bg-white border-[1px] py-[3.69%] px-[3.96%] border-black rounded-[22px]">
-            <input type="text" className="bg-red-200 w-[100%]" />
+            <input
+              type="text"
+              className="bg-red-200 w-[100%]"
+              value={isValue}
+              onChange={handleInput}
+            />
           </div>
           <div className="mt-[10px]">
-            <span className="--error-font-Color --Pretendard --medium text-[14px]">
-              *올바르게 작성해주세요.
-            </span>
+            {isState ? (
+              <span className="--error-font-Color --Pretendard --medium text-[14px]">
+                *올바르게 작성해주세요.
+              </span>
+            ) : (
+              <div className="--error-font-Color --Pretendard --medium text-[14px] mb-[24px]" />
+            )}
           </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center w-full">
-        <div className="flex items-center justify-center w-full --Pretendard --semi-bold --font-xl --status-bg-Color-07 py-[clamp(0px,3.3%,25px)] rounded-[30px]">
+        <div
+          className={`flex items-center justify-center w-full --Pretendard --semi-bold --font-xl ${isButton ? '--primary-bg-Color' : '--status-bg-Color-07'} py-[clamp(0px,3.3%,25px)] rounded-[30px]`}
+        >
           가입 하기
           <img className="ml-[16px]" src={loginImage} alt="img" />
         </div>
-        <div className="--status-font-Color-04 border-[--status-font-Color-04] border-b-[1px] --Pretendard --medium --font-xs mt-[clamp(0px,1.98%,15px)]">
+        <div className="--status-font-Color-04 border-[--status-font-Color-04] border-b-[1px] --Pretendard --medium --font-xs mt-[clamp(0px,1.98%,15px)] cursor-pointer">
           이미 만들어진 공장이 있어요!
         </div>
       </div>
