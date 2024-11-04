35c573f14c0a89c2b6f31d12240247bc12094929
4Bee <4bee.code@gmail.com>
2024-11-04 21:31:11 +0900

    ✨: 닉네임 작성 페이지 동적 버튼 동적으로 구현

--- front/src/View/Login/Login.nickname.component.tsx
+++ front/src/View/Login/Login.nickname.component.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import loginBlackImage from '../DEV/img/login-black-logo.svg';
 
 const LoginNickName = () => {
+  const [isState, setState] = useState(false);
+  const [isButton, setButton] = useState(false);
+  const [isValue, setValue] = useState('');
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValue(value);
+    console.log(value.length);
+    if (value.length >= 6) {
+      setState(true);
+      setButton(false);
+    } else if (value.length === 0) {
+      setState(false);
+      setButton(false);
+    } else if (value.length === 5) {
+      setState(false);
+      setButton(true);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-full pt-[12.01%] pb-[clamp(0px,10.39%,56px)] bg-yellow-300">
       <div className="flex flex-col justify-center items-center bg-orange-500 mb-[49px]">
@@ -19,21 +39,32 @@ const LoginNickName = () => {
         </div>
         <div className="flex flex-col w-full --Pretendard">
           <div className="--bold --font-m mb-[15px]">
-            공장을 위한 아이디가 필요해요!
+            공장을 위한 닉네임이 필요해요!
             <span className="--error-font-Color">*</span>
           </div>
           <div className="bg-white border-[1px] py-[3.69%] px-[3.96%] border-black rounded-[22px]">
-            <input type="text" className="bg-red-200 w-[100%]" />
+            <input
+              type="text"
+              className="bg-red-200 w-[100%]"
+              value={isValue}
+              onChange={handleInput}
+            />
           </div>
           <div className="mt-[10px]">
-            <span className="--error-font-Color --Pretendard --medium text-[14px]">
-              *너무 길어요! 올바르게 작성해주세요.
-            </span>
+            {isState ? (
+              <span className="--error-font-Color --Pretendard --medium text-[14px]">
+                *너무 길어요! 올바르게 작성해주세요.
+              </span>
+            ) : (
+              <div className="--error-font-Color --Pretendard --medium text-[14px] mb-[20px]" />
+            )}
           </div>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center w-full">
-        <div className="flex items-center justify-center w-full --Pretendard --semi-bold --font-xl --status-bg-Color-07 py-[clamp(0px,3.3%,25px)] rounded-[30px]">
+        <div
+          className={`flex items-center justify-center w-full --Pretendard --semi-bold --font-xl ${isButton ? '--primary-bg-Color' : '--status-bg-Color-07'} py-[clamp(0px,3.3%,25px)] rounded-[30px] ${isButton ? 'cursor-pointer' : ''}`}
+        >
           가입 하기
           <img className="ml-[16px]" src={loginBlackImage} alt="img" />
         </div>
