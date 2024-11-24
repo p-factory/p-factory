87ae63f0348d5b271b94d2bba191e3af879b5ab2
[nyun-nye] <[2001_11_7@naver.com]>
2024-11-24 21:30:54 +0900

    ✨: Div에 포커스가 가도록 설정

--- front/src/View/VocabularyBook/components/VocabularyBookWord.component.tsx
+++ front/src/View/VocabularyBook/components/VocabularyBookWord.component.tsx
@@ -30,38 +30,38 @@ const VocabularyBookWord = ({
     <div
       className={`relative grid grid-cols-[1fr_auto_1fr] ${
         wordLine === 2 ? 'h-[188px]' : 'h-[76px]'
-      }  border-[0.9px] border-solid rounded-[18px] ${wordDelete ? 'text-[#e8e8e8] border-[#e8e8e8]' : 'border-[#959595]'}`}
+      }  border-[0.9px] border-solid rounded-[18px] ${
+        wordDelete ? 'text-[#e8e8e8] border-[#e8e8e8]' : 'border-[#959595]'
+      }`}
     >
-      <div className="pl-[clamp(0px,28.97%,84px)] pt-[23px] flex w-3/4">
-        <div className="w-full break-all">
-          <span
-            className={`${wordHighlight ? '--primary-bg-Color' : ''}`}
-            contentEditable={isEditable} // 상태에 따라 contentEditable 속성 변경
-            suppressContentEditableWarning // React 경고 제거
-            onBlur={handleUnEditable} // 포커스가 벗어날 때 이벤트 핸들러
-          >
+      <div
+        className="pl-[clamp(0px,28.97%,84px)] pt-[23px] flex"
+        contentEditable={isEditable} // 상태에 따라 contentEditable 속성 변경
+        suppressContentEditableWarning // React 경고 제거
+      >
+        <div className="w-3/4 break-all">
+          <span className={`${wordHighlight ? '--primary-bg-Color' : ''}`}>
             {word}
           </span>
         </div>
       </div>
       <div
-        className={`h-full border-l-[0.9px] ${wordDelete ? 'border-[#e8e8e8]' : 'border-[#959595]'}`}
+        className={`h-full border-l-[0.9px] ${
+          wordDelete ? 'border-[#e8e8e8]' : 'border-[#959595]'
+        }`}
       />
       <div
         className="flex justify-center pt-[23px]"
         contentEditable={isEditable} // 상태에 따라 contentEditable 속성 변경
         suppressContentEditableWarning // React 경고 제거
-        onBlur={handleUnEditable} // 포커스가 벗어날 때 이벤트 핸들러
       >
-        뜻
+        <span>뜻</span>
       </div>
       <img
         src={`${wordDelete ? pencilIconGrey : pencilIcon}`}
         alt="Pencil Icon"
         className="absolute top-[23px] right-[40px] w-[22px] h-[22px] cursor-pointer"
-        onClick={() => {
-          handleEditable();
-        }}
+        onClick={() => (isEditable ? handleUnEditable() : handleEditable())}
       />
       {wordDelete && (
         <div className="absolute flex items-center justify-center w-full">
