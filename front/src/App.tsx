417b8647ddcd6a5bb9dd63acee2e4ba381105437
4Bee <4bee.code@gmail.com>
2024-12-11 20:42:26 +0900

    🔧: package 파일 수정 및 React index.html 루트 tsx파일명 변경

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,8 @@
 import { useRecoilState } from 'recoil';
 import { countState } from './Model/Atoms/atoms';
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.scss';
+import assets from './assets/assets';
 
 const App = () => {
   // Recoil 상태로 관리
@@ -12,10 +12,10 @@ const App = () => {
     <div className='App'>
       <div>
         <a href='https://vitejs.dev' target='_blank'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
+          <img src={assets.viteLogo} className='logo' alt='Vite logo' />
         </a>
         <a href='https://reactjs.org' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+          <img src='/react.svg' className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
