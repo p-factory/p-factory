755609d81ea7b09d341ae10bd7b7a65e6e1b9e3f
4Bee <4bee.code@gmail.com>
2024-12-11 17:33:55 +0900

    🔧: front web build 테스트 확인 및 build 디렉토리 설정

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // 빌드 결과물이 생성될 디렉토리
+    sourcemap: false, // 소스맵 생성 여부
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // 코드 분할 설정
+      },
+    },
+  },
+});
