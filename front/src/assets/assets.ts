5f91794d3fd24d508284c2817978c512406e040f
4Bee <4bee.code@gmail.com>
2024-12-11 20:42:42 +0900

    🔧: 객체로 관리할 이미지 설정

---
+++ front/src/assets/assets.ts
@@ -0,0 +1,9 @@
+import reactLogo from './img/react.svg';
+import viteLogo from '/vite.svg'; // public 폴더 파일은 루트 경로로 접근 가능
+
+const assets = {
+  reactLogo,
+  viteLogo,
+};
+
+export default assets;
