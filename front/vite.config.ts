3e73dc92a21cacb8b4bdc64bf2cb726fe00feb6f
4Bee <4bee.code@gmail.com>
2024-12-14 00:20:27 +0900

    🔧: 추가 react-narive를 호환하기 위한 vite-plugin-react-native-web, react-native-web 플러그인과 라이브러리 설치

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import reactNativeWeb from 'vite-plugin-react-native-web';
-import path from 'path';
+// import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
